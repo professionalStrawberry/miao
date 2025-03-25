7a511851f67f1fd974b5e08fae3f3ebe74bbf59f
Peiying <111759862+professionalStrawberry@users.noreply.github.com>
2025-03-25 18:58:06 +0800

    Add files via upload

---
+++ js/map.js
@@ -0,0 +1,16 @@
+var map = L.map('map').setView([31.2323437, 121.4691024], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
